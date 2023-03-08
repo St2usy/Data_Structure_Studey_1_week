b1d61ae33e55a371177a5ef222e635972905234e
ST2USSY <sco6503@naver.com>
2023-03-08 21:04:41 +0900

    third commit

---
+++ 03장-CheckMatching.cpp
@@ -0,0 +1,13 @@
+#include "Stack_Made_2.h"
+#include <iostream>
+
+bool checkMatching(const char* checkstr) {
+	ArrayStack stack;
+}
+
+int main() {
+
+	checkMatching("[{(Cheol eon Sin)}]");
+
+	return 0;
+}
--- Main.cpp
+++ Main.cpp
@@ -1,25 +1,25 @@
-#include "Stack_Made_2.h"
-#include "Student_Stack.h"
-
-int main() {
-	//ArrayStack stack;
-	//for (int i = 0; i < 10; i++) {
-	//	stack.push(i);
-	//}
-	//stack.display();
-	//stack.pop();
-	//stack.pop();
-	//stack.pop();
-	//stack.display();
-
-	StudentStack stack2;
-	stack2.push(Student(2219410, "sin", "computer"));
-	stack2.push(Student(22194101, "kim", "computer"));
-	stack2.push(Student(22194102, "lee", "computer"));
-	stack2.display();
-	stack2.pop();
-	stack2.display();
-
-	return 0;
-}
-
+//#include "Stack_Made_2.h"
+//#include "Student_Stack.h"
+//
+//int main() {
+//	ArrayStack stack;
+//	for (int i = 0; i < 10; i++) {
+//		stack.push(i);
+//	}
+//	stack.display();
+//	stack.pop();
+//	stack.pop();
+//	stack.pop();
+//	stack.display();
+//
+//	//StudentStack stack2;
+//	//stack2.push(Student(2219410, "sin", "computer"));
+//	//stack2.push(Student(22194101, "kim", "computer"));
+//	//stack2.push(Student(22194102, "lee", "computer"));
+//	//stack2.display();
+//	//stack2.pop();
+//	//stack2.display();
+//
+//	return 0;
+//}
+//
